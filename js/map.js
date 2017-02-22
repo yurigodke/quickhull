6db203feca1434b6f59cd6635a162dbbf49feb79
Yuri Godke <ynunes@uolinc.com>
2017-02-22 08:02:44 -0300

    Update map example to canvas map

--- js/map.js
+++ js/map.js
@@ -1,40 +1,104 @@
 (function () {
-    window.map = new google.maps.Map(document.getElementById('map'), {
-        center: {
-            lat: -23.113790,
-            lng: -46.553230
-        },
-        zoom: 14
-    })
+  map = document.getElementById("map");
 
-    var points = new Array();
-    google.maps.event.addListener(map, 'click', function (event) {
-        var marker = new google.maps.Marker({
-            position: event.latLng,
-            map: map
-        })
-        points.push({
-            lat: event.latLng.lat(),
-            lng: event.latLng.lng(),
-        });
+  mapContext = map.getContext("2d")
 
-        var polyline = new getConvexHull(points);
+  mapWidth = map.offsetWidth;
+  mapHeight = map.offsetHeight;
 
-        if (window.poligons) {
-            window.poligons.setPath(polyline.getHullPoints(true));
-        } else {
+  points = [];
 
-            window.poligons = new google.maps.Polygon({
-                paths: polyline.getHullPoints(true),
-                strokeColor: '#FF0000',
-                strokeOpacity: 0.8,
-                strokeWeight: 2,
-                fillColor: '#FF0000',
-                fillOpacity: 0.35
-            })
+  drawLines();
 
-            poligons.setMap(map);
-        }
+  map.addEventListener("click",function(e){
+    resetCanvas();
+    var point = {
+      x: e.offsetX,
+      y: e.offsetY
+    }
 
-    });
+    points.push(point);
+
+    drawLines();
+    drawPoints();
+
+    if(points.length > 1){
+      drawConections();
+    }
+  })
 })(document, window);
+
+function drawPoints(){
+  for(var i=0;i<points.length;i++){
+    mapContext.beginPath();
+    mapContext.arc(points[i].x,points[i].y,2,0,2*Math.PI);
+    mapContext.fill();
+  }
+}
+
+
+function drawLines(){
+  var lines = 20;
+
+  for(var i=1;i<lines;i++){
+    var posY = (mapHeight/lines)*i;
+    var posX = (mapWidth/lines)*i;
+
+    var lineXStart = {
+      x: 0,
+      y: posY
+    }
+    var lineXEnd = {
+      x: mapWidth,
+      y: posY
+    }
+    var lineYStart = {
+      x: posX,
+      y: 0
+    }
+    var lineYEnd = {
+      x: posX,
+      y: mapHeight
+    }
+
+    mapContext.beginPath();
+    mapContext.strokeStyle = "#888";
+
+    mapContext.lineWidth = 1;
+    mapContext.moveTo(lineXStart.x,lineXStart.y);
+    mapContext.lineTo(lineXEnd.x,lineXEnd.y);
+    mapContext.stroke();
+
+    mapContext.lineWidth = 1;
+    mapContext.moveTo(lineYStart.x,lineYStart.y);
+    mapContext.lineTo(lineYEnd.x,lineYEnd.y);
+    mapContext.stroke();
+  }
+}
+
+function drawConections(){
+  var polyline = new Quickhull(points);
+
+  var pointsConection = polyline.getHullPoints(true);
+
+  console.log(points);
+  console.log(pointsConection);
+
+  mapContext.beginPath();
+  mapContext.strokeStyle = "#333333";
+  for(var i=0;i<pointsConection.length;i++){
+    if(i==0){
+      mapContext.moveTo(pointsConection[i].x,pointsConection[i].y);
+    } else if(i == pointsConection.length-1){
+      mapContext.lineTo(pointsConection[i].x,pointsConection[i].y);
+      mapContext.lineTo(pointsConection[0].x,pointsConection[0].y);
+    } else {
+      mapContext.lineTo(pointsConection[i].x,pointsConection[i].y);
+    }
+  }
+  mapContext.stroke();
+}
+
+function resetCanvas(){
+  mapContext.clearRect(0,0,mapWidth,mapHeight);
+}
